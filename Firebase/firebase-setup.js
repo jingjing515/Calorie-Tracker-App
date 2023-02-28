1d2743881b8669f319eb7b29ddb2f64cbdaa3ccd
jingjing515 <majingjing515@outlook.com>
2023-02-27 23:32:34 -0800

    set up environment variables

--- Firebase/firebase-setup.js
+++ Firebase/firebase-setup.js
@@ -1,17 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import React from "react";
+import {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+} from "@env";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCtaOqdDIAkoZc7kd62iUvIuvFLN8VhXVQ",
-  authDomain: "myapp-2e85c.firebaseapp.com",
-  projectId: "myapp-2e85c",
-  storageBucket: "myapp-2e85c.appspot.com",
-  messagingSenderId: "356809760841",
-  appId: "1:356809760841:web:a29756801308eee2b29f98",
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
 };
 
 // Initialize Firebase
